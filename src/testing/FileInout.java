99d33e28d7b4dc665c49b41442de6fe129ab4203
d-qoi <alex@d-qoi.com>
2015-07-26 22:45:37 -0700

    Moving files around

--- src/testing/FileInout.java
+++ src/testing/FileInout.java
@@ -1,9 +1,15 @@
-package playerIn;
+package testing;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayDeque;
 import java.util.Scanner;
+
+import org.apache.commons.io.input.TailerListener;
+import org.apache.commons.io.input.TailerListenerAdapter;
+
+import playerIn.Message;
+import playerIn.PlayerIn;
 
 
 public class FileInout extends PlayerIn {
@@ -15,6 +21,7 @@ public class FileInout extends PlayerIn {
 		this.fileIn = messageFileIn;
 		this.fileOut = messageFileOut;
 		messages = new ArrayDeque<>();
+		
 	}
 	
 	@Override
@@ -36,7 +43,7 @@ public class FileInout extends PlayerIn {
 		
 	}
 	
-	private class readFileIn implements Runnable {
+	private class ReadFileIn implements Runnable {
 
 		@Override
 		public void run() {
@@ -48,7 +55,7 @@ public class FileInout extends PlayerIn {
 		
 	}
 	
-	private class writeFileOut implements Runnable {
+	private class WriteFileOut implements Runnable {
 
 		@Override
 		public void run() {
@@ -56,6 +63,14 @@ public class FileInout extends PlayerIn {
 			
 		}
 		
+	}
+	
+	private class Tailerhandler extends TailerListenerAdapter {
+		public void handle(String line) {
+			String[] message = line.split(":: ");
+			assert(message.length == 2);
+			messages.offer(new Message(message[0], message[1]));
+		}
 	}
 	
 }
